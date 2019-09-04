93e5766857886acee4050b227b8e4769e0608895
Tikam Chand <tikamchand06@gmail.com>
2019-09-04 12:58:15 +0530

    Improvements

--- js/background.js
+++ js/background.js
@@ -1,33 +1,29 @@
 ﻿let browser;
-if (navigator.userAgent.indexOf("Chrome") !== -1){
-    browser = chrome;   
+if (navigator.userAgent.indexOf('Chrome') !== -1) {
+  browser = chrome;
 }
 
-browser.runtime.onMessage.addListener(function(request,sender,sendResponse) {
-	if(request.URL){
-		sendAjax(request.URL, sendResponse);
-		return true;
-	}
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.URL) {
+    sendAjax(request.URL, sendResponse);
+    return true;
+  }
 });
 
-function sendAjax(url, callback){
-    let apiKey = 'apiKey=c56e5d3aa3616d1d1348';
-    let API_URL = 'https://free.currencyconverterapi.com/api/v6/'+ url + ((url == 'currencies') ? '?' : '&') + apiKey;
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", API_URL, true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            callback && typeof callback == 'function' && callback(JSON.parse(xhr.responseText))
-        }
+function sendAjax(url, callback) {
+  let apiKey = 'apiKey=c56e5d3aa3616d1d1348';
+  let API_URL = 'https://free.currencyconverterapi.com/api/v6/' + url + (url == 'currencies' ? '?' : '&') + apiKey;
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', API_URL, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      callback && typeof callback == 'function' && callback(JSON.parse(xhr.responseText));
     }
-    xhr.send( null );
+  };
+  xhr.send(null);
 }
 
-function getCurrencies(){
-    sendAjax('currencies', function(response){
-        localStorage.currencies = JSON.stringify(response);
-    })
-}
+const getCurrencies = () => sendAjax('currencies', response => (localStorage.currencies = JSON.stringify(response)));
 
 // Get Currencies
 chrome.runtime.onStartup.addListener(getCurrencies);
--- js/popup.js
+++ js/popup.js
@@ -1,105 +1,96 @@
 let browser, selectize;
-if (navigator.userAgent.indexOf("Chrome") !== -1){
-    browser = chrome;   
+if (navigator.userAgent.indexOf('Chrome') !== -1) {
+  browser = chrome;
 }
 
-$(function(){
-    // Get Currencies List
-    getCurrencies(function(response){
-        let listOptions = [];
-        if(response.results){
-            $.each(response.results, function(key, item){
-                let currencyItem = {
-                    id: key,
-                    title: item.currencyName + (item.currencySymbol ? ' (' + item.currencySymbol+')' : '')
-                };
-                listOptions.push(currencyItem);
-            });
-        }
-        
-        selectize = $('.currency-list').selectize({
-            valueField: 'id',
-            labelField: 'title',
-            searchField: 'title',
-            options: listOptions,
-            closeAfterSelect: true
-        });
-        
-        // Set Default Currencies
-        setDefaultCurrencies();
-    });
-
-    $('#currentYear').text(new Date().getFullYear());
-    $('#settings').on('click', function(){
-        $('#settings_modal').show();
-    })
-    $('#cancelbtn').on('click', function(){
-        $('#settings_modal').hide();
-    })
-});
-
-function setDefaultCurrencies(){
-    if(localStorage.defaultCurrencies){
-        let dc = JSON.parse(localStorage.defaultCurrencies);
-        $.each(selectize, function(i, item){
-            let defaultValue = item.id == 'fromCurrency' || item.id == 'fromCurrency1' ? dc.fromCurrency : dc.toCurrency;
-            item.selectize.setValue(defaultValue);
-        })
+$(() => {
+  // Get Currencies List
+  getCurrencies(response => {
+    let listOptions = [];
+    if (response.results) {
+      $.each(response.results, function(key, item) {
+        let currencyItem = {
+          id: key,
+          title: item.currencyName + (item.currencySymbol ? ' (' + item.currencySymbol + ')' : '')
+        };
+        listOptions.push(currencyItem);
+      });
     }
-}
 
-$('#settings_form').on('submit', function(e){
-    e.preventDefault();
-
-    let defaultCurrencies = {
-        fromCurrency: $('#fromCurrency1').val(),
-        toCurrency: $('#toCurrency1').val()
-    };
-
-    localStorage.defaultCurrencies = JSON.stringify(defaultCurrencies);
+    selectize = $('.currency-list').selectize({
+      valueField: 'id',
+      labelField: 'title',
+      searchField: 'title',
+      options: listOptions,
+      closeAfterSelect: true
+    });
 
     // Set Default Currencies
-    if($('#apply_changes').is(':checked')){
-        setDefaultCurrencies();
-    }
+    setDefaultCurrencies();
+  });
 
-    $('#settings_modal').hide();
-    alert('Default Currencies Saved!');
-})
-
-$(document).on('click', '#reset-currency', function(){
-    $('.result-container').fadeOut();
-})
-
-// Convert Currency
-$(document).on("submit", '#convert-currency-form', function(e){
-    e.preventDefault();
-    $('.result-container').fadeIn();
-
-    var amount = $('#amount').val();
-    var from = $('#fromCurrency').val();
-    var to = $('#toCurrency').val();
-    var q = from+'_'+to;
-    
-    sendAjaxToApi({URL: 'convert?q='+q}, function(response){
-        if(response.results){
-            var val = response.results[q].val;
-            var result = ((val*amount*100)/100).toFixed(2);
-            $('.result-container').html('<strong>Result: </strong>'+ result +' '+ to);
-        }
-    });
+  $('#currentYear').text(new Date().getFullYear());
+  $('#settings').on('click', () => $('#settings_modal').show());
+  $('#cancelbtn').on('click', () => $('#settings_modal').hide());
 });
 
-function getCurrencies(callback){
-    if(localStorage.currencies){
-        callback(JSON.parse(localStorage.currencies))
-    } else {
-        sendAjaxToApi({URL: 'currencies'}, callback)
-    }
+function setDefaultCurrencies() {
+  if (localStorage.defaultCurrencies) {
+    let dc = JSON.parse(localStorage.defaultCurrencies);
+    $.each(selectize, function(i, item) {
+      let defaultValue = item.id == 'fromCurrency' || item.id == 'fromCurrency1' ? dc.fromCurrency : dc.toCurrency;
+      item.selectize.setValue(defaultValue);
+    });
+  }
 }
 
-function sendAjaxToApi(messageData, callback){
-    browser.runtime.sendMessage(messageData, function(response){
-        callback && typeof callback == 'function' && callback(response)
-    })
+$('#settings_form').on('submit', e => {
+  e.preventDefault();
+
+  let defaultCurrencies = {
+    fromCurrency: $('#fromCurrency1').val(),
+    toCurrency: $('#toCurrency1').val()
+  };
+
+  localStorage.defaultCurrencies = JSON.stringify(defaultCurrencies);
+
+  // Set Default Currencies
+  if ($('#apply_changes').is(':checked')) setDefaultCurrencies();
+
+  $('#settings_modal').hide();
+});
+
+$(document).on('click', '#reset-currency', () => $('.result-container').fadeOut());
+
+// Convert Currency
+$(document).on('submit', '#convert-currency-form', e => {
+  e.preventDefault();
+  $('.result-container').fadeIn();
+
+  var amount = $('#amount').val();
+  var from = $('#fromCurrency').val();
+  var to = $('#toCurrency').val();
+  var q = from + '_' + to;
+
+  sendAjaxToApi({ URL: 'convert?q=' + q }, response => {
+    if (response.results) {
+      var val = response.results[q].val;
+      var result = ((val * amount * 100) / 100).toFixed(2);
+      $('.result-container').html('<strong>Result: </strong>' + result + ' ' + to);
+    }
+  });
+});
+
+function getCurrencies(callback) {
+  if (localStorage.currencies) {
+    callback(JSON.parse(localStorage.currencies));
+  } else {
+    sendAjaxToApi({ URL: 'currencies' }, callback);
+  }
+}
+
+function sendAjaxToApi(messageData, callback) {
+  browser.runtime.sendMessage(messageData, function(response) {
+    callback && typeof callback == 'function' && callback(response);
+  });
 }
